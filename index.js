43e1fd99f70fc1585219579651de2bb2ed25b340
Fritz Lin <uxfritz@163.com>
2017-09-12 21:28:15 +0800

    1.0.1 - lower captcha difficulty

--- index.js
+++ index.js
@@ -26,28 +26,30 @@ function verifyCaptcha (ctx) {
 //制造验证码图片
 function makeCaptcha() {
     var img = new BMP24(100, 40);
+
+    img.fillRect(0, 0, 100, 40, 0xf8f8f8);
     img.drawCircle(rand(0, 100), rand(0, 40), rand(10 , 40), rand(0, 0xffffff));
     //边框
-    img.drawRect(0, 0, img.w-1, img.h-1, rand(0, 0xffffff));
-    img.fillRect(rand(0, 100), rand(0, 40), rand(10, 35), rand(10, 35), rand(0, 0xffffff));
+    // img.drawRect(0, 0, img.w-1, img.h-1, rand(0, 0xffffff));
+    // img.fillRect(rand(0, 100), rand(0, 40), rand(10, 35), rand(10, 35), rand(0, 0xffffff));
     img.drawLine(rand(0, 100), rand(0, 40), rand(0, 100), rand(0, 40), rand(0, 0xffffff));
     //return img;
 
     //画曲线
-    var w=img.w/2;
-    var h=img.h;
-    var color = rand(0, 0xffffff);
-    var y1=rand(-7,7); //Y轴位置调整
-    var w2=rand(15,20); //数值越小频率越高
-    var h3=rand(6,8); //数值越小幅度越大
-    var bl = rand(1,5);
-    for(var i=-w; i<w; i+=0.1) {
-        var y = Math.floor(h/h3*Math.sin(i/w2)+h/2+y1);
-        var x = Math.floor(i+w);
-        for(var j=0; j<bl; j++){
-            img.drawPoint(x, y+j, color);
-        }
-    }
+    // var w=img.w/2;
+    // var h=img.h;
+    // var color = rand(0, 0xffffff);
+    // var y1=rand(-7,7); //Y轴位置调整
+    // var w2=rand(15,20); //数值越小频率越高
+    // var h3=rand(6,8); //数值越小幅度越大
+    // var bl = rand(1,5);
+    // for(var i=-w; i<w; i+=0.1) {
+    //     var y = Math.floor(h/h3*Math.sin(i/w2)+h/2+y1);
+    //     var x = Math.floor(i+w);
+    //     for(var j=0; j<bl; j++){
+    //         img.drawPoint(x, y+j, color);
+    //     }
+    // }
 
     var p = "ABCDEFGHKMNPQRSTUVWXYZ3456789";
     var str = '';
@@ -55,7 +57,8 @@ function makeCaptcha() {
         str += p.charAt(Math.random() * p.length |0);
     }
 
-    var fonts = [BMP24.font8x16, BMP24.font12x24, BMP24.font16x32];
+    // var fonts = [BMP24.font8x16, BMP24.font12x24, BMP24.font16x32];
+    var fonts = [BMP24.font12x24, BMP24.font16x32];
     var x = 15, y=8;
     for(var i=0; i<str.length; i++){
         var f = fonts[Math.random() * fonts.length |0];
